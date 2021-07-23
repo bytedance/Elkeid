89615079a518a25e8cba9fd17d4d13896b063bcb
zhanglei.sec <zhanglei.sec@bytedance.com>
2021-07-23 16:56:11 +0800

    collecor version:1.6.0.1.

--- agent/collector/main.go
+++ agent/collector/main.go
@@ -31,7 +31,7 @@ type container struct {
 
 func main() {
 	// Connect to agent
-	c, err := libmongoose.Connect("../../plugin.sock", "collector", "1.6.0.0")
+	c, err := libmongoose.Connect("../../plugin.sock", "collector", "1.6.0.1")
 	if err != nil {
 		zap.S().Error(err)
 		return
