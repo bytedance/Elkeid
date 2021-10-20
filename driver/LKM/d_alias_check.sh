fea0ffc1fa1181789a2afc35dcb960253a6c6039
chenyue.will <chenyue.will@bytedance.com>
2021-10-20 12:52:12 +0800

    use d_alias_check replace centos_cehck

--- driver/LKM/d_alias_check.sh
+++ driver/LKM/d_alias_check.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-grep -A 2 'd_u;' /lib/modules/$KVERSION/build/include/linux/dcache.h | grep -c d_alias;
+if [ -f "/lib/modules/$KVERSION/build/include/linux/dcache.h" ];then
+  grep -A 2 'd_u;' /lib/modules/$KVERSION/build/include/linux/dcache.h | grep -c d_alias;
+else
+  grep -A 2 'd_u;' /usr/src/kernels/$KVERSION/include/linux/dcache.h | grep -c d_alias;
+fi
