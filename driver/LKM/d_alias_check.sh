5d28d7e99344e5174cbc9cbba16c4b58d9042ac6
chenyue.will <chenyue.will@bytedance.com>
2021-10-20 17:01:27 +0800

    update d_alias_check.sh;cancel build dir check

--- driver/LKM/d_alias_check.sh
+++ driver/LKM/d_alias_check.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ -f "$KERNEL_DIR/build/include/linux/dcache.h" ];then
-  grep -A 2 'd_u;' $KERNEL_DIR/build/include/linux/dcache.h | grep -c d_alias;
-else
-  grep -A 2 'd_u;' $KERNEL_DIR/include/linux/dcache.h | grep -c d_alias;
-fi
+grep -A 2 'd_u;' $KERNEL_DIR/include/linux/dcache.h | grep -c d_alias;
