b4695b2b48070c058908ba3966ba93405e5c1566
Bach <kulukami@gmail.com>
2022-04-18 18:33:44 +0800

    add python3-pip deps

--- plugins/scanner_clamav/get_deps.sh
+++ plugins/scanner_clamav/get_deps.sh
@@ -2,9 +2,9 @@
 
 # make static lib for clamav deps
 apt-get update 
-apt-get install build-essential clang llvm cmake
-apt-get install libstdc++6 libstdc++-6-dev
-apt-get install -y flex bison python3-dev pkg-config ninja-build
+apt-get install -y build-essential clang llvm cmake
+apt-get install -y libstdc++6 libstdc++-6-dev
+apt-get install -y flex bison python3-dev pkg-config ninja-build python3-pip
 python3 -m pip install mussels
 
 git clone --depth 1 https://github.com/Cisco-Talos/clamav-mussels-cookbook.git
