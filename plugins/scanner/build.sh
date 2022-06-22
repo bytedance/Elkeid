7c48277d1bea91904b94d61ddad8ef1acd08b65a
Bach <kulukami@gmail.com>
2022-06-22 11:37:09 +0800

    update scanner musl

--- plugins/scanner/build.sh
+++ plugins/scanner/build.sh
@@ -13,7 +13,7 @@ rm -rf output/* &> /dev/null
 mkdir output &> /dev/null
 
 # build plugin
-RUSTFLAGS='-C target-feature=+crt-static ' cargo build --release --bin scanner_plugin --target x86_64-unknown-linux-gnu
+CC="x86_64-linux-musl-gcc" CXX="x86_64-linux-musl-c++" RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --bin scanner_plugin --target x86_64-unknown-linux-musl
 
 if [ $? -ne 0 ]; then
     echo "etrace cli build failed"
@@ -22,11 +22,11 @@ else
     echo "etrace cli build succeed"
 fi
 
-cp  target/x86_64-unknown-linux-gnu/release/scanner_plugin  ./output/scanner
+cp  target/x86_64-unknown-linux-musl/release/scanner_plugin  ./output/scanner
 strip ./output/scanner
 
 # build cli
-RUSTFLAGS='-C target-feature=+crt-static ' cargo build --release --bin scanner_cli --target x86_64-unknown-linux-gnu
+CC="x86_64-linux-musl-gcc" CXX="x86_64-linux-musl-c++" RUSTFLAGS='-C target-feature=+crt-static'  cargo build --release --bin scanner_cli --target x86_64-unknown-linux-musl
 
 if [ $? -ne 0 ]; then
     echo "etrace cli build failed"
@@ -35,13 +35,15 @@ else
     echo "etrace cli build succeed"
 fi
 
-cp  target/x86_64-unknown-linux-gnu/release/scanner_cli  ./output/scanner_cli
+cp  target/x86_64-unknown-linux-musl/release/scanner_cli  ./output/scanner_cli
 strip ./output/scanner_cli
+
+cp -r tools/* ./output/.
 
 cd ./output 
 mkdir tmp 
 wget http://lf9-elkeid.bytetos.com/obj/elkeid-download/18249e0cbe7c6aca231f047cb31d753fa4604434fcb79f484ea477f6009303c3/archive_db_default_20220607.zip
 mv archive_db_default_20220607.zip ./tmp
-tar zcvf scanner.tar.gz scanner tmp
+tar zcvf scanner.tar.gz scanner tmp elkeid_targets
 cd - 
 
