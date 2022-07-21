55fe4fa48940fa4c09a9f6bf70aee25b74947c85
Bach <kulukami@gmail.com>
2022-07-21 19:43:32 +0800

    fix elrepo 2

---
+++ driver/batch_compile_elrepo6.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+mkdir -p /ko_output
+BUILD_VERSION=$(cat LKM/src/init.c | grep MODULE_VERSION | awk -F '"' '{print $2}')
+KO_NAME=$(grep "MODULE_NAME" ./LKM/Makefile | grep -m 1 ":=" | awk '{print $3}')
+
+for each_ml_version in `curl http://mirrors.coreix.net/elrepo-archive-archive/kernel/el6/x86_64/RPMS/ | grep el6.elrepo.x86_64.rpm | grep kernel-ml-devel | sed -r 's/.*href="([^"]+).*/\1/g' | sed -r 's/kernel-ml-devel-([^"]+).el6.elrepo.x86_64.rpm/\1/g'`
+do 
+    wget "http://mirrors.coreix.net/elrepo-archive-archive/kernel/el6/x86_64/RPMS/kernel-ml-devel"-$each_ml_version.el6.elrepo.x86_64.rpm
+    
+    yum remove -y kernel-devel kernel-ml-devel kernel-lt-devel
+    
+    rpm -ivh --force ./kernel*.rpm 
+    rm -f ./kernel*.rpm 
+    KV=$each_ml_version.el6.elrepo.x86_64
+    KVERSION=$KV make -C ./LKM clean || true 
+    BATCH=true KVERSION=$KV make -C ./LKM -j all || true 
+     sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
+    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true 
+    KVERSION=$KV  make -C ./LKM clean || true
+done
+
+for each_lt_version in `curl http://mirrors.coreix.net/elrepo-archive-archive/kernel/el6/x86_64/RPMS/ | grep el6.elrepo.x86_64.rpm | grep kernel-lt-devel | sed -r 's/.*href="([^"]+).*/\1/g' | sed -r 's/kernel-lt-devel-([^"]+).el6.elrepo.x86_64.rpm/\1/g'`
+do 
+    wget "http://mirrors.coreix.net/elrepo-archive-archive/kernel/el6/x86_64/RPMS/kernel-lt-devel"-$each_lt_version.el6.elrepo.x86_64.rpm
+    
+    yum remove -y kernel-devel kernel-ml-devel kernel-lt-devel
+    rpm -ivh --force ./kernel*.rpm 
+
+    rm -f ./kernel*.rpm 
+    KV=$each_lt_version.el6.elrepo.x86_64
+    KVERSION=$KV make -C ./LKM clean || true 
+    BATCH=true KVERSION=$KV make -C ./LKM -j all || true 
+     sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
+    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true 
+    KVERSION=$KV  make -C ./LKM clean || true
+done
