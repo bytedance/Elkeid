1da998bbf3ea031bd9773ca2be9e511d7e21fd6d
A.z fan <kulukami@gmail.com>
2022-11-17 11:03:05 +0800

    Main driver compile (#351)
    * update centos plus
    
    * add tmp build
    
    * tmp revert 1.7.0.4
    
    * fix centos7 plus
    
    * fix centos7 plus docker file
    
    * fix centos7 & 8 centosplus
    
    * fix centos8 plus
    
    * prepare for merge
    
    * update build flow, remove dead log
    
    * add actions debug
    
    * FIXME: building failure for kernel-ml-4.15.0-1.el7.elrepo.x86_64
    
    Error "No rule to make target" resolved: module building for RHEL7 kernel-ml-4.15.0-1.el7.elrepo.x86_64.
    Besides this patch, need copy objtool to header directory for kernel-ml-4.15.0-1.el7.elrepo.
    
    * add hot fix
    
    * tmp build centos6
    
    * update build log & release
    
    * save ci
    
    * update release ci
    
    * update ci
    
    * add missing versions
    
    * add missing ubuntu version
    
    * disable download ssl check
    
    * download mode wget
    
    * pass headers-install through succeed
    
    * test vms
    
    * add wget ssl pass check
    
    * fix ubuntu old release domain ssl, done
    
    * add ubuntu18 missing wget & curl
    
    * add missing objtool
    
    * add centos 4.15 missing objtool
    
    * update version into 1.7.0.8
    
    * update driver ci
    
    * update tlsbin
    
    * add missing dockerfile.x86_64
    
    * update CI
    
    * update CI docker/build-push-action@v3
    
    * fix rl8 rl8plus
    
    * CI without submodule
    
    * update CI docker/build-push-action@v3
    
    * fix driver/build_script/aarch64/batch_compile_el7.sh
    
    * fix aarch64 el7 disableGcc8
    
    * fix aarch64/batch_compile_el7.sh
    
    * add more el7.aarch64
    
    * fix missing pcilibs
    
    * fix aarch64 sh
    
    * fix aarch64 sh
    
    * update 1.7.0.9
    
    Co-authored-by: shenping-bd <83050026+shenping-bd@users.noreply.github.com>

---
+++ driver/build_script/x86_64/batch_compile_el7.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mkdir -p /ko_output
+BUILD_VERSION=$(cat LKM/src/init.c | grep MODULE_VERSION | awk -F '"' '{print $2}')
+KO_NAME=$(grep "MODULE_NAME" ./LKM/Makefile | grep -m 1 ":=" | awk '{print $3}')
+
+
+for each_tag in `yum --showduplicates list kernel-devel | grep kernel-devel | awk -c '{print $2}'`
+do 
+    yumdownloader  --destdir /root/headers kernel-devel-$each_tag.x86_64 > /dev/null
+    rpm --force -i /root/headers/kernel-devel-$each_tag.x86_64.rpm || true
+    KV=$each_tag.x86_64
+    
+    KVERSION=$KV make -C ./LKM clean || true 
+    BATCH=true KVERSION=$KV make -C ./LKM -j all | tee /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.log || true 
+    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
+
+    if [ -s /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign ]; then
+        # The file is not-empty.
+        echo ok > /dev/null
+    else
+        # The file is empty.
+        rm -f /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign
+    fi
+    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true 
+    KVERSION=$KV  make -C ./LKM clean || true
+
+    rpm --force -e kernel-devel-$each_tag.x86_64 > /dev/null || true
+    rm -f /root/headers/*.rpm
+done
