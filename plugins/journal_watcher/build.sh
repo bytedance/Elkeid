6c644621414bf869bc8189567825d285b5f9a8f4
Serinalice <mrzhang.lei@outlook.com>
2022-11-29 10:53:06 +0800

    feat: update journal_watcher to 1.9.1.

--- plugins/journal_watcher/build.sh
+++ plugins/journal_watcher/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-BUILD_VERSION="1.0.0.10"
+if [ -z "${BUILD_VERSION}" ];then
+        echo 'please set BUILD_VERSION'
+        exit 1
+fi
 mkdir -p output
 RUSTFLAGS='-C link-arg=-s' cargo build --target x86_64-unknown-linux-musl --release
+RUSTFLAGS='-C link-arg=-s' cargo build --target aarch64-unknown-linux-musl --release
 cp target/x86_64-unknown-linux-musl/release/journal_watcher output/journal_watcher-linux-amd64-${BUILD_VERSION}.plg
+cp target/aarch64-unknown-linux-musl/release/journal_watcher output/journal_watcher-linux-arm64-${BUILD_VERSION}.plg
