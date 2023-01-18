5fed12687ba58ea7ad599a1a213ed62ff4d4fc92
kulukami <kulukami@gmail.com>
2023-01-18 10:22:19 +0800

    fix: ko CI tencentOS

--- driver/build_script/x86_64/batch_compile_tencentos.sh
+++ driver/build_script/x86_64/batch_compile_tencentos.sh
@@ -3,14 +3,13 @@ mkdir -p /ko_output
 BUILD_VERSION=$(cat LKM/src/init.c | grep MODULE_VERSION | awk -F '"' '{print $2}')
 KO_NAME=$(grep "MODULE_NAME" ./LKM/Makefile | grep -m 1 ":=" | awk '{print $3}')
 
+UBUNTU_OR_DEBIAN_FLAG=$(cat /etc/*release | grep -iE "ubuntu|debian")
+FLAG_SIZE=${#UBUNTU_OR_DEBIAN_FLAG}
 
 for each_tag in `yum --showduplicates list kernel-devel | grep kernel-devel | awk -c '{print $2}'`; 
 do 
     rpm -e kernel-devel
     yum -y install  kernel-devel-$each_tag.x86_64; 
-
-    KV=${each_tag%.*}
-    KVERSION=$KV make -C ./LKM clean || true 
 
     if [ -z $CC ];then
         export CC=gcc
@@ -18,21 +17,26 @@ do
     echo "$CC --version =>"
     $CC --version
 
-    BATCH=true KVERSION=$KV make -C ./LKM -j all | tee /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.log || true 
-    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
+    for f in /usr/src/kernels/*
+    do
+        set -e
+        set -o xtrace
+        KV="$(basename -- $f)"
+        echo "Processing $KV file..."
+        KVERSION=$KV  make -C ./LKM clean || true
+        BATCH=true KVERSION=$KV make -C ./LKM -j all | tee /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.log || true 
+        sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
 
-    BATCH=true KVERSION=$KV make -C ./LKM -j all | tee /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.log || true 
-    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign  || true  
-
-    if [ -s /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign ]; then
-        # The file is not-empty.
-        echo ok > /dev/null
-    else
-        # The file is empty.
-        rm -f /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign
-    fi
-    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true 
-    KVERSION=$KV  make -C ./LKM clean || true
+        if [ -s /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign ]; then
+            # The file is not-empty.
+            echo ok > /dev/null
+        else
+            # The file is empty.
+            rm -f /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.sign
+        fi
+        mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_amd64.ko || true
+        KVERSION=$KV  make -C ./LKM clean || true
+    done
 
 done
 
