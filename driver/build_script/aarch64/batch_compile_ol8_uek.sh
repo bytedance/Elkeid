63bfe6d4aef728cb1ca92a235ba9d48605a71d8f
kulukami <kulukami@gmail.com>
2023-06-20 14:19:40 +0800

    update driver ci for el8

---
+++ driver/build_script/aarch64/batch_compile_ol8_uek.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+mkdir -p /ko_output
+BUILD_VERSION=$(cat LKM/src/init.c | grep MODULE_VERSION | awk -F '"' '{print $2}')
+KO_NAME=$(grep "MODULE_NAME" ./LKM/Makefile | grep -m 1 ":=" | awk '{print $3}')
+
+
+enableGcc11(){
+    export CC=/opt/rh/gcc-toolset-11/root/usr/bin/gcc
+    export CPP=/opt/rh/gcc-toolset-11/root/usr/bin/cpp
+    export CXX=/opt/rh/gcc-toolset-11/root/usr/bin/c++
+}
+
+enableGcc11
+
+echo "this is centos"
+for f in /usr/src/kernels/*
+do
+    set -e
+    set -o xtrace
+    KV="$(basename -- $f)"
+    echo "Processing $KV file..."
+    $CC --version
+    KVERSION=$KV  make -C ./LKM clean || true
+    BATCH=true KVERSION=$KV make -C ./LKM -j all | tee /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_arm64.log || true 
+    sha256sum  ./LKM/${KO_NAME}.ko | awk '{print $1}' > /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_arm64.sign  || true  
+
+    if [ -s /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_arm64.sign ]; then
+            # The file is not-empty.
+            echo ok > /dev/null
+    else
+            # The file is empty.
+            rm -f /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_arm64.sign
+    fi
+    mv ./LKM/${KO_NAME}.ko /ko_output/${KO_NAME}_${BUILD_VERSION}_${KV}_arm64.ko || true
+    KVERSION=$KV  make -C ./LKM clean || true
+done
