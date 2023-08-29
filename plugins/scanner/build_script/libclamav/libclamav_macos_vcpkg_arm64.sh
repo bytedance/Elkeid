2e5b7392252ac47406abbe9ff1414b9ad91347d4
kulukami <kulukami@gmail.com>
2023-08-29 11:35:23 +0800

    update libclamav_ci

---
+++ plugins/scanner/build_script/libclamav/libclamav_macos_vcpkg_arm64.sh
@@ -0,0 +1,79 @@
+export VCPKG_DEFAULT_TRIPLET="arm64-osx-static"
+export RUSTC_TARGET="aarch64-apple-darwin"
+export TARGET_ARCH="arm64"
+
+rustup target add $RUSTC_TARGET
+
+git clone https://github.com/kulukami/vcpkg.git
+export VCPKG_ROOT="$(pwd)/vcpkg"
+export VCPKG_INSTALLATION_ROOT="$(pwd)/vcpkg"
+
+bash ./vcpkg/bootstrap-vcpkg.sh
+
+$VCPKG_ROOT/vcpkg install \
+openssl:$VCPKG_DEFAULT_TRIPLET \
+json-c:$VCPKG_DEFAULT_TRIPLET \
+pcre2:$VCPKG_DEFAULT_TRIPLET \
+zlib:$VCPKG_DEFAULT_TRIPLET \
+bzip2:$VCPKG_DEFAULT_TRIPLET \
+libiconv:$VCPKG_DEFAULT_TRIPLET \
+libxml2:$VCPKG_DEFAULT_TRIPLET 
+
+export VCPKG_INSTALL_PATH="$VCPKG_ROOT/installed/$VCPKG_DEFAULT_TRIPLET"
+
+git clone https://github.com/kulukami/clamav -b rel/1.1_yara_hit
+
+cd clamav
+mkdir build
+cd build
+cmake ..  \
+  -D CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" \
+  -D VCPKG_TARGET_TRIPLET="$VCPKG_DEFAULT_TRIPLET"                       \
+  -D VCPKG_TARGET_ARCHITECTURE="$TARGET_ARCH"                            \
+  -D RUST_COMPILER_TARGET="$RUSTC_TARGET"                                \
+  -D CMAKE_SYSTEM_NAME=Darwin                                            \
+  -D CMAKE_OSX_ARCHITECTURES="arm64"                                     \
+  -D ENABLE_TESTS=OFF                                                    \
+  -D ENABLE_STATIC_LIB=ON                                                \
+  -D ENABLE_LIBCLAMAV_ONLY=ON                                            \
+  -D ENABLE_SYSTEMD=OFF                                                  \
+  -D ENABLE_SHARED_LIB=OFF                                               \
+  -D BYTECODE_RUNTIME=none                                               \
+  -D ENABLE_UNRAR=ON                                                     \
+  -D ENABLE_FUZZ=OFF                                                     \
+  -D ENABLE_APP=OFF                                                      \
+  -D ENABLE_CLAMONACC=OFF                                                \
+  -D ENABLE_MILTER=OFF                                                   \
+  -D ENABLE_MAN_PAGES=OFF                                                \
+  -D CMAKE_INSTALL_PREFIX="install"
+
+cmake --build . --config Release --target install -j3
+
+cd ../..
+
+rm -rf lib
+mkdir lib
+
+cp clamav/build/libclamav/libclamav_static.a ./lib
+cp clamav/build/libclammspack/libclammspack_static.a ./lib
+cp clamav/build/libclamunrar/libclamunrar_static.a ./lib
+cp clamav/build/libclamunrar_iface/libclamunrar_iface_static.a ./lib
+cp clamav/build/$RUSTC_TARGET/release/libclamav_rust.a ./lib
+cp clamav/build/*.a ./lib
+
+cp $VCPKG_ROOT/installed/$VCPKG_DEFAULT_TRIPLET/lib/*.a ./lib
+
+rm -rf ./include/*
+mkdir include 
+cp clamav/build/*.h ./include
+cp clamav/libclamav/clamav.h ./include
+cp clamav/libclamav/matcher.h ./include
+cp clamav/libclamav/matcher-ac.h ./include
+cp clamav/libclamav/others.h ./include
+cp -r $VCPKG_ROOT/installed/$VCPKG_DEFAULT_TRIPLET/include/* ./include
+
+mkdir output
+mv include output
+mv lib output
+
+lipo -info output/lib/*.a
---
+++ plugins/scanner/build_script/libclamav/libclamav_windows_vcpkg_arm64.ps1
@@ -0,0 +1,74 @@
+$VCPKG_DEFAULT_TRIPLET="arm64-windows-static"
+$RUSTC_TARGET="aarch64-pc-windows-msvc"
+$TARGET_ARCH="arm64"
+
+rustup target add $RUSTC_TARGET
+$env:VCPKG_DEFAULT_TRIPLET=$VCPKG_DEFAULT_TRIPLET
+
+vcpkg install `
+curl[openssl]:$VCPKG_DEFAULT_TRIPLET `
+json-c:$VCPKG_DEFAULT_TRIPLET `
+libxml2:$VCPKG_DEFAULT_TRIPLET `
+pcre2:$VCPKG_DEFAULT_TRIPLET `
+pthreads:$VCPKG_DEFAULT_TRIPLET `
+zlib:$VCPKG_DEFAULT_TRIPLET `
+pdcurses:$VCPKG_DEFAULT_TRIPLET  `
+bzip2:$VCPKG_DEFAULT_TRIPLET `
+check:$VCPKG_DEFAULT_TRIPLET
+
+$VCPKG_ROOT="C:\vcpkg"
+$VCPKG_INSTALL_PATH="$VCPKG_ROOT\installed\$VCPKG_DEFAULT_TRIPLET"
+$env:VCPKG_INSTALL_PATH="$VCPKG_INSTALL_PATH"
+$env:VCPKGRS_DYNAMIC=0
+
+git clone https://github.com/kulukami/clamav -b rel/1.1_yara_hit
+
+cd clamav
+mkdir build
+cd build
+cmake .. `
+  -D CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" `
+  -D VCPKG_TARGET_TRIPLET="$VCPKG_DEFAULT_TRIPLET"                       `
+  -D VCPKG_TARGET_ARCHITECTURE="$TARGET_ARCH"                            `
+  -D RUST_COMPILER_TARGET="$RUSTC_TARGET"                                `
+  -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded                            `
+  -D ENABLE_TESTS=OFF                                                    `
+  -D ENABLE_STATIC_LIB=ON                                                `
+  -D ENABLE_LIBCLAMAV_ONLY=ON                                            `
+  -D ENABLE_SYSTEMD=OFF                                                  `
+  -D ENABLE_SHARED_LIB=OFF                                               `
+  -D BYTECODE_RUNTIME=none                                               `
+  -D ENABLE_UNRAR=ON                                                     `
+  -D ENABLE_FUZZ=OFF                                                     `
+  -D ENABLE_APP=OFF                                                      `
+  -D ENABLE_CLAMONACC=OFF                                                `
+  -D ENABLE_MILTER=OFF                                                   `
+  -D ENABLE_MAN_PAGES=OFF                                                `
+  -D CMAKE_INSTALL_PREFIX="install"
+
+
+cmake --build . --config Release --target install -j2
+
+
+cd ../..
+mkdir lib
+cp clamav\build\libclamav\Release\libclamav_static.lib .\lib
+cp clamav\build\libclammspack\Release\libclammspack_static.lib .\lib
+cp clamav\build\libclamunrar\Release\libclamunrar_static.lib .\lib
+cp clamav\build\libclamunrar_iface\Release\libclamunrar_iface_static.lib .\lib
+cp clamav\build\win32\compat\Release\libwin32_compat.lib .\lib
+cp clamav\build\install\*.dll .\lib
+cp clamav\build\install\*.lib .\lib
+cp $VCPKG_INSTALL_PATH\lib\*.lib .\lib
+
+mkdir include 
+cp clamav\libclamav\clamav.h .\include
+cp clamav\libclamav\matcher.h .\include
+cp clamav\libclamav\matcher-ac.h .\include
+cp clamav\libclamav\others.h .\include
+cp clamav\build\install\include\*.h .\include
+cp -r C:\vcpkg\installed\$VCPKG_DEFAULT_TRIPLET\include\* .\include
+
+mkdir output
+mv include output
+mv lib output
