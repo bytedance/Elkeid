4fa9e2f65bde3e28df2eeb090db3d76543eb8deb
kulukami <kulukami@gmail.com>
2023-09-25 16:57:34 +0800

    fix ci

--- plugins/scanner/docker/build_linux_x86_64.sh
+++ plugins/scanner/docker/build_linux_x86_64.sh
@@ -7,18 +7,18 @@ mv /LibClamavDocker/clamav .
 mv /LibClamavDocker/clamav-mussels-cookbook .
 mv /LibClamavDocker/lib .
 mv /LibClamavDocker/include .
-rustup target add aarch64-unknown-linux-musl
+rustup target add x86_64-unknown-linux-musl
 
-CC="aarch64-linux-musl-gcc" CXX="aarch64-linux-musl-c++" RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --bin scanner_plugin --target aarch64-unknown-linux-musl
+CC="x86_64-linux-musl-gcc" CXX="x86_64-linux-musl-c++" RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --bin scanner_plugin --target x86_64-unknown-linux-musl
 
 mkdir -p output/tmp
 cp -r tools/* ./output/.
 cp settings.toml ./output/.
 
-cp target/aarch64-unknown-linux-musl/release/scanner_plugin output/scanner
+cp target/x86_64-unknown-linux-musl/release/scanner_plugin output/scanner
 strip output/scanner
 
 wget http://lf9-elkeid.bytetos.com/obj/elkeid-download/18249e0cbe7c6aca231f047cb31d753fa4604434fcb79f484ea477f6009303c3/archive_db_default_20221206.zip
 mv archive_db_default_20221206.zip ./output/tmp
 
-cd output && tar zcvf scanner-aarch64.tar.gz scanner tmp elkeid_targets settings.toml && cd -
+cd output && tar zcvf scanner-x86_64.tar.gz scanner tmp elkeid_targets settings.toml && cd -
