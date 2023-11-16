62677b1e42f3e7af0c593dddfb67a7d9c4f0d970
guoyj <seyy.mail@163.com>
2023-11-16 17:33:30 +0800

    Add Rule Receive Code

---
+++ rasp/jvm/JVMProbe/src/main/java/com/security/smith/client/Rule_Version.java
@@ -0,0 +1,17 @@
+package com.security.smith.client;
+
+public class Rule_Version {
+    private  int rule_version;
+
+    public  Rule_Version() {
+
+    }
+
+    public int getRule_version() {
+        return this.rule_version;
+    }
+
+    public void setRule_version(int rule_version) {
+        this.rule_version = rule_version;
+    }
+}
