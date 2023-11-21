94f887ea65b567587895cbff4ff03dcb61708f1c
yoloyyh <1764163852@qq.com>
2023-11-21 14:37:11 +0800

    feat class_filter_version when report heartbeat

--- rasp/jvm/JVMProbe/src/main/java/com/security/smith/client/Rule_Version.java
+++ rasp/jvm/JVMProbe/src/main/java/com/security/smith/client/Rule_Version.java
@@ -2,6 +2,7 @@ package com.security.smith.client;
 
 public class Rule_Version {
     private  int rule_version;
+    private String class_filter_version;
 
     public  Rule_Version() {
 
@@ -13,5 +14,13 @@ public class Rule_Version {
 
     public void setRule_version(int rule_version) {
         this.rule_version = rule_version;
+    }
+
+    public String getClass_filter_version() {
+        return this.class_filter_version;
+    }
+
+    public void setClass_filter_version(String class_filter_version) {
+        this.class_filter_version = class_filter_version;
     }
 }
