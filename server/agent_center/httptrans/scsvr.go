5da00d0740579b2b08f8c7011c2cfe03b5af83bf
yaopengbo <yaopengbo@bytedance.com>
2024-05-28 11:32:08 +0800

    ypb_tmp_ac

--- server/agent_center/httptrans/scsvr.go
+++ server/agent_center/httptrans/scsvr.go
@@ -1,12 +1,15 @@
 package httptrans
 
 import (
+	"crypto/tls"
 	"fmt"
 	"github.com/bytedance/Elkeid/server/agent_center/common"
 	"github.com/bytedance/Elkeid/server/agent_center/common/ylog"
 	"github.com/bytedance/Elkeid/server/agent_center/httptrans/http_handler"
 	"github.com/bytedance/Elkeid/server/agent_center/httptrans/midware"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func Run() {
@@ -30,14 +33,40 @@ func runServer(port int, enableSSL, enableAuth bool, certFile, keyFile string) {
 		apiGroup.POST("/command/", http_handler.PostCommand) //Post commands to the agent
 	}
 
-	var err error
 	ylog.Infof("RunServer", "####HTTP_LISTEN_ON:%d", port)
 	if enableSSL {
-		err = router.RunTLS(fmt.Sprintf(":%d", port), certFile, keyFile)
+		secCipherSuites := make([]uint16, 0, 0)
+		for _, c := range tls.CipherSuites() {
+			secCipherSuites = append(secCipherSuites, c.ID)
+		}
+
+		// 创建自定义的TLS配置
+		tlsConfig := &tls.Config{
+			// 禁用不安全的加密套件
+			CipherSuites:     secCipherSuites,
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		}
+
+		// 创建http.Server实例
+		server := &http.Server{
+			Addr:      fmt.Sprintf(":%d", port),
+			Handler:   router,
+			TLSConfig: tlsConfig, // 将自定义的TLS配置应用到http.Server上
+		}
+
+		if err := server.ListenAndServeTLS(certFile, keyFile); err != nil {
+			if err == http.ErrServerClosed {
+				// 服务器被正常关闭
+				log.Printf("Server closed: %v", err)
+			} else {
+				ylog.Errorf("RunServer", "####http run error: %v", err)
+			}
+		}
 	} else {
-		err = router.Run(fmt.Sprintf(":%d", port))
-	}
-	if err != nil {
-		ylog.Errorf("RunServer", "####http run error: %v", err)
+		err := router.Run(fmt.Sprintf(":%d", port))
+		if err != nil {
+			ylog.Errorf("RunServer", "####http run error: %v", err)
+		}
 	}
 }
