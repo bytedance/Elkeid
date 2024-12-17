51b8da802039364cbec16b220040d7b24c427d2c
kulukami <kulukami@gmail.com>
2024-12-17 12:31:45 +0800

    revert: cgroup-rs version

--- plugins/scanner/src/lib.rs
+++ plugins/scanner/src/lib.rs
@@ -187,9 +187,9 @@ pub fn setup_cgroup(pid: u32, mem: i64, cpu: i64) -> Result<()> {
         .cpu()
         .quota(cpu) //  n / MAX 100 000 = x% CPU
         .done()
-        .build(hier1)?;
+        .build(hier1);
 
-    scanner_cg.add_task(cgroups_rs::CgroupPid::from(pid as u64))?;
+    scanner_cg.add_task(cgroups_rs::CgroupPid::from(pid as u64));
     return Ok(());
 }
 
