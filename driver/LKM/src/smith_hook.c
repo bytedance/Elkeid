5e917406f235833943131b99f84b54c2a5bf4eb6
shenping.matt <shenping.matt@bytedance.com>
2025-06-10 16:04:36 +0800

    Improvement: using kprobe for commit_creds
    don't use kretprobe hooking, and use kprobe or tracepoint instead
    
    Signed-off-by: shenping.matt <shenping.matt@bytedance.com>

--- driver/LKM/src/smith_hook.c
+++ driver/LKM/src/smith_hook.c
@@ -826,15 +826,6 @@ struct execve_data {
     int free_ld_preload;
 };
 
-struct update_cred_data {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 5, 0)
-    uid_t old_uid;
-#else
-    int old_uid;
-#endif
-};
-
-
 /*
  * Our own implementation of kernel_getsockname and kernel_getpeername,
  * resuing codes logic of kernel inet_getname and inet6_getname.
@@ -3803,45 +3794,36 @@ static int do_init_module_pre_handler(struct kprobe *p, struct pt_regs *regs)
     return 0;
 }
 
-static int update_cred_entry_handler(struct kretprobe_instance *ri,
-                              struct pt_regs *regs)
+static int update_cred_pre_handler(struct kprobe *p, struct pt_regs *regs)
 {
-    struct update_cred_data *data;
-    data = (struct update_cred_data *)ri->data;
-    data->old_uid = __get_current_uid();
-    return 0;
-}
-
-static int update_cred_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-    int now_uid;
-    int retval;
     char *exe_path = NULL;
     char *buffer = NULL;
     char *pid_tree = NULL;
-    struct update_cred_data *data;
+    struct cred *cred;
+    int new_uid, old_uid;
 
-    now_uid = __get_current_uid();
-    retval = regs_return_value(regs);
-
-    //only get old uid ≠0 && new uid == 0
-    if (now_uid != 0)
+    cred = (void *)p_regs_get_arg1(regs);
+    if (IS_ERR_OR_NULL(cred))
         return 0;
 
-    data = (struct update_cred_data *)ri->data;
-    if (data->old_uid != 0) {
-        buffer = smith_kzalloc(PATH_MAX, GFP_ATOMIC);
-        exe_path = smith_get_exe_file(buffer, PATH_MAX);
+    new_uid = _XID_VALUE(cred->uid);
+    old_uid = __get_current_uid();
 
-        pid_tree = smith_get_pid_tree(PID_TREE_LIMIT);
-        update_cred_print(exe_path, pid_tree, data->old_uid, retval);
+    // only report if old uid ≠0 && new uid == 0
+    if (new_uid != 0 || old_uid == 0)
+        return 0;
 
-        if (buffer)
-            smith_kfree(buffer);
+    buffer = smith_kzalloc(PATH_MAX, GFP_ATOMIC);
+    exe_path = smith_get_exe_file(buffer, PATH_MAX);
 
-        if (pid_tree)
-            smith_kfree(pid_tree);
-    }
+    pid_tree = smith_get_pid_tree(PID_TREE_LIMIT);
+    update_cred_print(exe_path, pid_tree, old_uid, 0);
+
+    if (buffer)
+        smith_kfree(buffer);
+    if (pid_tree)
+        smith_kfree(pid_tree);
+
     return 0;
 }
 
@@ -4048,11 +4030,9 @@ static struct kprobe do_init_module_kprobe = {
         .pre_handler = do_init_module_pre_handler,
 };
 
-static struct kretprobe update_cred_kretprobe = {
-        .kp.symbol_name = "commit_creds",
-        .data_size = sizeof(struct update_cred_data),
-        .handler = update_cred_handler,
-        .entry_handler = update_cred_entry_handler,
+static struct kprobe update_cred_kprobe = {
+        .symbol_name = "commit_creds",
+        .pre_handler = update_cred_pre_handler,
 };
 
 static struct kprobe security_inode_create_kprobe = {
@@ -4559,7 +4539,7 @@ static void unregister_prctl_kprobe(void)
 static int register_update_cred_kprobe(void)
 {
     int ret;
-    ret = smith_register_kretprobe(&update_cred_kretprobe);
+    ret = register_kprobe(&update_cred_kprobe);
     if (ret == 0)
         update_cred_kprobe_state = 0x1;
 
@@ -4568,7 +4548,7 @@ static int register_update_cred_kprobe(void)
 
 static void unregister_update_cred_kprobe(void)
 {
-    smith_unregister_kretprobe(&update_cred_kretprobe);
+    unregister_kprobe(&update_cred_kprobe);
 }
 
 static int register_mprotect_kprobe(void)
